b9b07196f9e2b88ce4e3b70725eadf1a1c2ab773
raqbrrs <raquelbrrs07@gmail.com>
2024-09-04 15:58:07 -0300

    média aritmetica

---
+++ atividade04.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética. 
+
+n1 = int(input("digite a nota 1"))
+n2 = int(input("digite a nota 2"))
+n3 = int(input("digite a nota 3"))
+
+média = (n1 + n2 + n3) / 3
+print (média)
